e44ef459448a93e7f5a10236d7ba473d218313a1
104233 <104233@U-55170.domena.uksw.edu.pl>
2019-10-26 12:28:12 +0200

    Program HELLO WORLD

---
+++ test/src/test/HelloWorld.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	
+	String str =  "HELLO";
+	
+	Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+	
+		String hello = "Hello world!";
+		
+		System.out.println("Napis 'Hello world!' ma " + hello.length() + " znakow.");
+		
+	}
+			
+}
